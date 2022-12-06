652b116d2f3b512f11b04dd80b02fcd500e65c4a
Neil <77575319+Armweak5104@users.noreply.github.com>
2022-12-06 16:25:53 +0100

    Add files via upload

---
+++ AOC_D4/D4P2.cpp
@@ -0,0 +1,35 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+int main(){
+  string content;
+  ifstream grouping("input4.txt");
+  string range1;
+  int range1l;
+  int range1u;
+  string range2;
+  int range2l;
+  int range2u;
+  int counter=0;
+  while (getline(grouping,content)){
+    range1 = content.substr(0,content.find(","));
+    range1l = stoi(range1.substr(0,range1.find("-")));
+    range1u = stoi(range1.substr(range1.find("-")+1));
+    range2 = content.substr(content.find(",")+1);
+    range2l = stoi(range2.substr(0,range2.find("-")));
+    range2u = stoi(range2.substr(range2.find("-")+1));
+    // cout<<range1<<endl;
+    // cout<<range1l<<endl;
+    // cout<<range1u<<endl;
+    // cout<<range2<<endl;
+    // cout<<range2l<<endl;
+    // cout<<range2u<<endl;
+    if (range1l<=range2u && range2l<=range1u){
+      counter+=1;
+  cout<<counter<<endl;
+    }
+  }
+}
