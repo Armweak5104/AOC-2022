4f4aa1c388e36da2fcd96d395b9e2d7c528ed7f2
Neil <77575319+Armweak5104@users.noreply.github.com>
2022-12-06 17:02:50 +0100

    Add files via upload

---
+++ AOC_D5/D5P1.cpp
@@ -0,0 +1,20 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+int main(){
+  vector<char> organization = {{},{},{},{},{},{},{},{},{}};
+  string content;
+  ifstream crane("input5.txt");
+  int i = 0;
+  while(getline(crane,content)){
+    /*
+Use the i variable to iterate through the first line and add the letter to the respective vector in the 2d vector - organization
+      */
+  }
+  /*
+Next use the instructions and from each line detect the numbers and then follow the instructions using the push_back() or pop() function
+  */
+}
