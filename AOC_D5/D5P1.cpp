1bec2290e4ba960eab27c2d074121541e849b520
Neil <77575319+Armweak5104@users.noreply.github.com>
2022-12-07 07:49:59 +0100

    Update D5P1.cpp

--- AOC_D5/D5P1.cpp
+++ AOC_D5/D5P1.cpp
@@ -5,16 +5,32 @@
 using namespace std;
 
 int main(){
-  vector<char> organization = {{},{},{},{},{},{},{},{},{}};
+  //Wasnt sure how to get the data so I hard coded it
+  vector<vector<char>> organization = {{'R','N','P','G'},{'T','J','B','L','C','S','V','H'},{'T','D','B','M','N','L'},{'R','V','P','S','B'},{'G','C','Q','S','W','M','V','H'},{'W','Q','S','C','D','B','J'},{'F','Q','L'},{'W','M','H','T','D','L','F','V'},{'L','P','B','V','M','J','F'}};
   string content;
   ifstream crane("input5.txt");
-  int i = 0;
+  int vol;
+  int start;
+  int dest;
+  string msg = "";
   while(getline(crane,content)){
-    /*
-Use the i variable to iterate through the first line and add the letter to the respective vector in the 2d vector - organization
-      */
+    if (content[0]=='m'){
+      vol = stoi(content.substr(content.find('e')+1,content.find('f')-content.find('e')-1));
+      start = stoi(content.substr(content.find('f')+5,content.find('t')-content.find('f')-5));
+      dest = stoi(content.substr(content.length()-1));
+      dest -=1;
+      start-=1;
+      int x = organization[start].size()-1;
+      for (int i = x; i>x-vol; --i){
+          organization[dest].push_back(organization[start][i]); 
+      }
+      for(int i = x; i>x-vol; --i){
+          organization[start].pop_back();
+      }
+    }
   }
-  /*
-Next use the instructions and from each line detect the numbers and then follow the instructions using the push_back() or pop() function
-  */
+  for (int i = 0; i< organization.size(); i++){
+   msg+=organization[i][organization[i].size()-1];
+  }
+  cout<<msg<<endl;
 }
