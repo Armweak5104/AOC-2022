3f5abaa8e43d0a9d8797283ebd9aff1168473910
Neil <77575319+Armweak5104@users.noreply.github.com>
2022-12-07 07:57:25 +0100

    Add files via upload

---
+++ AOC_D5/D5P2.cpp
@@ -0,0 +1,40 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+int main(){
+  //Wasnt sure how to get the data so I hard coded it
+  vector<vector<char>> organization = {{'R','N','P','G'},{'T','J','B','L','C','S','V','H'},{'T','D','B','M','N','L'},{'R','V','P','S','B'},{'G','C','Q','S','W','M','V','H'},{'W','Q','S','C','D','B','J'},{'F','Q','L'},{'W','M','H','T','D','L','F','V'},{'L','P','B','V','M','J','F'}};
+  string content;
+  ifstream crane("input5.txt");
+  int vol;
+  int start;
+  int dest;
+  string msg = "";
+  while(getline(crane,content)){
+    if (content[0]=='m'){
+      vol = stoi(content.substr(content.find('e')+1,content.find('f')-content.find('e')-1));
+      start = stoi(content.substr(content.find('f')+5,content.find('t')-content.find('f')-5));
+      dest = stoi(content.substr(content.length()-1));
+      dest -=1;
+      start-=1;
+      int x = organization[start].size()-1;
+      for (int i = x-vol+1; i<x+1; ++i){
+          organization[dest].push_back(organization[start][i]); 
+      }
+      for(int i = x; i>x-vol; --i){
+          organization[start].pop_back();
+      }
+    }
+    cout<<organization[0][organization[0].size()-1]<<endl;
+    cout<<organization[1][organization[1].size()-1]<<endl;
+    cout<<organization[2][organization[2].size()-1]<<endl;
+    cout<<"----------"<<endl;
+  }
+  for (int i = 0; i< organization.size(); i++){
+   msg+=organization[i][organization[i].size()-1];
+  }
+  cout<<msg<<endl;
+}
